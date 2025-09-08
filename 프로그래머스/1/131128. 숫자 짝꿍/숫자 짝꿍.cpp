34028a3e5727457cb86b4833bbf1a51d4aaea94f
TyXD <dncka22@naver.com>
2025-09-08 10:20:12 +0900

    [level 1] Title: 숫자 짝꿍, Time: 80.45 ms, Memory: 29 MB -BaekjoonHub

--- 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
+++ 프로그래머스/1/131128. 숫자 짝꿍/숫자 짝꿍.cpp
@@ -1,53 +1,22 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <algorithm>
+#include <map>
 
 using namespace std;
 
-string solution(string X, string Y) {
-    string answer = "";
-    
-    priority_queue<char> qX;
-    priority_queue<char> qY;
-    
-    for (auto& x : X)
+string solution(string X, string Y)
+{
+    string answer="";
+    map<char,int> mx;
+    for(char c : X) mx[c]++;
+    map<char,int> my;
+    for(char c : Y) my[c]++;
+    for(int i=9; i>=0; i--)
     {
-        qX.push(x);
+        int num=min(mx[i+'0'],my[i+'0']);
+        for(int j=0; j<num; j++) answer+=i+'0';
     }
-    for (const auto& y : Y)
-    {
-        qY.push(y);
-    }
-    
-    while(!qX.empty() && !qY.empty())
-    {
-        if(qX.top() == qY.top())
-        {
-            if(answer == "" && qX.top() == '0')
-            {
-                answer = answer + qX.top();
-                break;
-            }
-            answer += qX.top();
-            qX.pop();
-            qY.pop();
-        }
-        else
-        {
-            if(qX.top() > qY.top())
-            {
-                qX.pop();
-            }
-            else
-            {
-                qY.pop();
-            }
-        }
-    }
-    if(answer == "")
-    {
-        answer = "-1";
-    }
+    if(answer=="") answer="-1";
+    else if(answer[0]=='0' && answer.size()>1) answer="0";
     return answer;
 }
