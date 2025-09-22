0fff07f3cba1ca40dcce30efe4b16caab714519b
TyXD <dncka22@naver.com>
2025-09-22 09:47:06 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <cctype>
+using namespace std;
+
+string solution(string s) {
+    string answer = "";
+    
+    char temp = ' ';
+    
+    for(auto& word : s)
+    {
+        if(temp == ' ' && islower(word))
+        {
+            word = toupper(word);
+        }
+        if(temp != ' ' && isupper(word))
+        {
+            word = tolower(word);
+        }
+        temp = word;
+    }
+    return s;
+}
