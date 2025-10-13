072264fc03fad285a542bad6c66c66251d9f3e85
TyXD <dncka22@naver.com>
2025-10-13 09:10:09 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.09 ms, Memory: 5.67 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    
+    for(int i = 0; i < arr1.size(); ++i)
+    {
+        vector<int> row;
+        for(int j = 0; j < arr2[0].size(); ++j)
+        {
+            int temp = 0;
+            for(int k = 0; k < arr1[0].size(); ++k)
+            {
+                temp += arr1[i][k] * arr2[k][j];
+            }
+            row.push_back(temp);
+        }
+        answer.push_back(row);
+    }
+    return answer;
+}
