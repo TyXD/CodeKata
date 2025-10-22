02e0c8a38fa00a3c5f32e02854320fafaee73f5b
TyXD <dncka22@naver.com>
2025-10-22 09:01:02 +0900

    [level 2] Title: 타겟 넘버, Time: 5.03 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -18,7 +18,6 @@ void dfs(vector<int>& numbers, int target, int sum, int index)
     }
     dfs(numbers, target, sum + numbers[index], index + 1);
     dfs(numbers, target, sum - numbers[index], index + 1);
-
 }
 int solution(vector<int> numbers, int target) 
 {
