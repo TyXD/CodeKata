6ef7e6a1b3c6d752ef20e19fbad67afe41b2e7b7
TyXD <dncka22@naver.com>
2025-12-10 10:11:59 +0900

    [level 2] Title: 모음 사전, Time: 0.02 ms, Memory: 4.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.cpp
@@ -4,41 +4,15 @@
 
 using namespace std;
 
-int answer = 0;
-int count = 0;
-char characters[5] = {'A', 'E', 'I', 'O', 'U'};
-
-void dfs(string answer_word, string word)
-{
-    if(answer != 0)
-    {
-        return;
+int solution(string word) {
+    string vowels = "AEIOU";
+    int weights[5] = {781, 156, 31, 6, 1};
+    int answer = 0;
+    
+    for(int i = 0; i < word.length(); ++i) {
+        int index = vowels.find(word[i]);
+        answer += index * weights[i] + 1;
     }
     
-    if(answer_word == word)
-    {
-        answer = count;
-        return;
-    }
-    
-    if(answer_word.size() >= 5)
-    {
-        return;
-    }
-    
-    for(int i = 0; i < 5; ++i)
-    {
-        count++;
-        dfs(answer_word+characters[i], word);
-    }
-}
-
-int solution(string word) 
-{
-    
-    answer = 0;
-    count = 0;
-    dfs("", word);
-
     return answer;
 }
