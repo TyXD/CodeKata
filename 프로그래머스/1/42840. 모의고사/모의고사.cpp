27713901ad1d0ba3e5a0d7f69df71ad13938ff4a
TyXD <dncka22@naver.com>
2025-12-10 10:09:05 +0900

    [level 1] Title: 모의고사, Time: 0.24 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.cpp
+++ 프로그래머스/1/42840. 모의고사/모의고사.cpp
@@ -4,40 +4,29 @@
 using namespace std;
 
 vector<int> solution(vector<int> answers) {
+    vector<vector<int>> patterns = {
+        {1,2,3,4,5},
+        {2,1,2,3,2,4,2,5},
+        {3,3,1,1,2,2,4,4,5,5}
+    };
+    
+    vector<int> scores(3);
+    
+    for(int i = 0; i < 3; ++i) {
+        scores[i] = count_if(answers.begin(), answers.end(), 
+            [&, i, idx = 0](int ans) mutable {
+                return ans == patterns[i][idx++ % patterns[i].size()];
+            });
+    }
+    
+    int max_score = *max_element(scores.begin(), scores.end());
+    
     vector<int> answer;
-    
-    vector<int> supo1 = {1,2,3,4,5};
-    vector<int> supo2 = {2,1,2,3,2,4,2,5};
-    vector<int> supo3 = {3,3,1,1,2,2,4,4,5,5};
-    
-    vector<int> supo = {0, 0, 0};
-    
-    for( int i = 0; i < answers.size(); ++i)
-    {
-        if(answers[i] == supo1[(i)%5])
-        {
-            supo[0]++;
-        }
-        if(answers[i] == supo2[(i)%8])
-        {
-            supo[1]++;
-        }
-        if(answers[i] == supo3[(i)%10])
-        {
-            supo[2]++;
+    for(int i = 0; i < 3; ++i) {
+        if(scores[i] == max_score) {
+            answer.push_back(i + 1);
         }
     }
-    int max_score = *max_element(supo.begin(), supo.end());
-    
-    for(int i = 0; i < supo.size(); ++i)
-    {
-        if(max_score == supo[i])
-        {
-            answer.push_back(i+1);
-        }
-    }
-    
-           
     
     return answer;
 }
